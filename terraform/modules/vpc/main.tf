f3c5582b4cda9cab71a342459dc957000333d96b
fxdev00 <fxvisione@gmail.com>
2026-04-14 20:51:41 +0100

    fix: changed router config in nat from id to name

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -37,7 +37,7 @@ resource "google_compute_router" "router" {
 resource "google_compute_router_nat" "nat" {
   name                               = "barberops-nat"
   region                             = var.region
-  router                             = google_compute_router.router.id
+  router                             = google_compute_router.router.name
   project                            = var.project_id
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
